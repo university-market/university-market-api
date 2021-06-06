ffe825dbe311ab09bb32e12e19d89e607f00d327
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-06-05 23:24:37 -0300

    model da entidade de publicacao

---
+++ application/app/Models/Publicacao.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Publicacao extends Model{
+    
+    public $timestamps = false; // Nao registrar data/hora criacao/alteracao
+    protected $table = 'Publicacao';
+    protected $primaryKey = 'publicacaoId';
+
+    protected $publicacaoId; // PK Publicacao
+    protected $usuarioId; // FK Tabela Usuario
+    protected $titulo;
+    protected $descricao;
+    protected $valor;
+    protected $pathImagem;
+    protected $dataHoraCriacao;
+    protected $dataHoraFinalizacao;
+    protected $dataHoraExclusao;
+}
