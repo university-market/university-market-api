ca6ef89b340960651d54de4b84f30a8ee9c04b6b
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-06-06 00:29:28 -0300

    model de detalhes da publicacao

---
+++ application/app/Http/Controllers/Publicacao/Models/PublicacaoDetalheModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Publicacao\Models;
+
+class PublicacaoDetalheModel {
+
+    public $publicacaoId;
+    public $titulo;
+    public $descricao;
+    public $valor;
+    public $pathImagem;
+    public $dataHoraCriacao;
+}
