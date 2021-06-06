0cc36888175bcd81c912c5e5872a1cbb19bbfb56
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-06-05 23:28:40 -0300

    model de criacao de publicacao

---
+++ application/app/Http/Controllers/Publicacao/Models/PublicacaoCriacaoModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Publicacao\Models;
+
+class PublicacaoCriacaoModel {
+
+    public $titulo;
+    public $descricao;
+    public $valor;
+    public $pathImagem;
+}
