8b49acbb945994a055bd569e4ebbfbf3b984d9f3
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-08 01:44:34 -0300

    metodo de cadastramento de instituicao

--- app/Http/Controllers/Instituicao/InstituicaoController.php
+++ app/Http/Controllers/Instituicao/InstituicaoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 // Models de conta utilizadas
+use App\Models\Instituicao;
 use App\Http\Controllers\Instituicao\Models\InstituicaoCriacaoModel;
 
 class InstituicaoController extends UniversityMarketController {
@@ -17,7 +18,36 @@ class InstituicaoController extends UniversityMarketController {
 
     $model->validar();
 
-    return response()->json($model);
+    // Validacao se ja existe um cadastro ativo para a instituicao
+    $hasCadastro = Instituicao::where('ativa', true)
+      ->where(
+        function($query) use ($model) {
+
+          $query->where('cnpj', $model->cnpj)
+            ->orWhere('razaoSocial', $model->razaoSocial)
+            ->orWhere('cpfRepresentante', $model->cpfRepresentante);
+        }
+      )->first();
+
+    if (!\is_null($hasCadastro))
+      throw new \Exception("Já existe um cadastro que corresponde aos dados informados");
+
+    $instituicao = new Instituicao();
+
+    $instituicao->nomeFantasia = $model->nomeFantasia;
+    $instituicao->razaoSocial = $model->razaoSocial;
+    $instituicao->nomeReduzido = null; // Cadastro posterior
+    $instituicao->cnpj = $model->cnpj;
+    $instituicao->cpfRepresentante = $model->cpfRepresentante;
+    $instituicao->emailContato = $model->emailContato;
+    $instituicao->etapaCadastroId = 1; // Solicitacao enviada
+    $instituicao->dataHoraCadastro = date($this->dateTimeFormat);
+    $instituicao->dataHoraAprovacao = null; // Somente quando aprovada
+    $instituicao->ativa = true; // Cadastro ativo
+
+    $instituicao->save();
+
+    return response()->json($instituicao->instituicaoId);
   }
 
 }
