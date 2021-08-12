4c88fa54ce292de8dec89ae4bfb9cb77e695fb5a
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-11 21:33:02 -0300

    insert da tabela de cursos

---
+++ database/scripts/Script09 - Insert de cursos.sql
@@ -0,0 +1,20 @@
+INSERT INTO courses (id, grid_cols, grid_rows, course_name, img_path) 
+VALUES (1, 2, 2, "Arquitetura", "assets/grid_img/arquitetura.jpeg");
+
+INSERT INTO courses (id, grid_cols, grid_rows, course_name, img_path) 
+VALUES (2, 1, 1, "Análise e desenvolvimento", "assets/grid_img/ads.jpg");
+
+INSERT INTO courses (id, grid_cols, grid_rows, course_name, img_path) 
+VALUES (3, 1, 2, "Odontologia", "assets/grid_img/odontologia.jpg");
+
+INSERT INTO courses (id, grid_cols, grid_rows, course_name, img_path) 
+VALUES (4, 1, 2, "Análise e desenvolvimento", "assets/grid_img/engenharia-civil.jpg");
+
+INSERT INTO courses (id, grid_cols, grid_rows, course_name, img_path) 
+VALUES (5, 1, 1, "Piscologia", "assets/grid_img/psicologia.jpg");
+
+INSERT INTO courses (id, grid_cols, grid_rows, course_name, img_path) 
+VALUES (6, 1, 1, "Fisioterapia", "assets/grid_img/fisioterapia.jpg");
+
+INSERT INTO courses (id, grid_cols, grid_rows, course_name, img_path) 
+VALUES (7, 1, 1, "Direito", "assets/grid_img/direito.jpg");
