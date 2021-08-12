431a335967ab70df578a0e73754e141013b53f7f
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-11 23:14:10 -0300

    model de entidade estudante

--- app/Models/Estudante.php
+++ app/Models/Estudante.php
@@ -15,12 +15,38 @@ class Estudante extends Model{
    */
   protected $casts = [
       'estudanteId' => 'integer',
+      'nome' => 'string',
+      'ra' => 'string',
+      'email' => 'string',
+      'telefone' => 'string',
+      'dataNascimento' => 'datetime',
+      'hashSenha' => 'string',
+      'pathFotoPerfil' => 'string',
+      'ativo' => 'boolean',
+      'blocked' => 'boolean',
+      'dataHoraFimBlock' => 'datetime',
+      'dataHoraCadastro' => 'datetime',
       'dataHoraExclusao' => 'datetime',
+      'cursoId' => 'integer',
+      'instituicaoId' => 'integer'
   ];
 
   protected $table = 'Estudante';
   protected $primaryKey = 'estudanteId';
 
   protected $estudanteId; // PK Estudante
+  protected $nome;
+  protected $ra;
+  protected $email;
+  protected $telefone;
+  protected $dataNascimento;
+  protected $hashSenha;
+  protected $pathFotoPerfil;
+  protected $ativo;
+  protected $blocked;
+  protected $dataHoraFimBlock;
+  protected $dataHoraCadastro;
   protected $dataHoraExclusao;
+  protected $cursoId; // FK Curso
+  protected $instituicaoId; // FK Instituicao
 }
