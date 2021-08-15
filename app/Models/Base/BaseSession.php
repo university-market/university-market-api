50f82092276178e23db6bb8c26837abc44fe2699
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-15 03:26:08 -0300

    create base session model

---
+++ app/Models/Base/BaseSession.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Base;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseSession extends Model {
+
+  public $timestamps = false; // Nao registrar data/hora criacao/alteracao
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'sessionId' => 'integer',
+    'usuarioId' => 'integer',
+    'sessionToken' => 'string',
+    'dataHoraExpiracao' => 'datetime',
+  ];
+
+  protected $primaryKey = 'sessionId';
+
+  protected $sessionId; // PK Session
+  protected $usuarioId; // FK Tabela Usuario
+  protected $sessionToken;
+  protected $dataHoraExpiracao;
+
+}
