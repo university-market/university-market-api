babe593e84f1362544bf0c0748f239773d959db1
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-15 03:47:06 -0300

    app login model

---
+++ app/Http/Controllers/Auth/Models/AppLoginModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Models;
+
+class AppLoginModel {
+
+  public $email;
+  public $senha;
+
+  public function validar() {
+
+    if (is_null($this->email) || empty($this->email))
+      throw new \Exception("Um e-mail deve ser informado");
+
+    if (is_null($this->senha) || empty($this->senha))
+      throw new \Exception("A senha é obrigatória");
+  }
+}
