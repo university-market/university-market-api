8e1020e0e6cdf0827b63627485c961dd55f25077
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-08-15 03:19:39 -0300

    update script app session

--- database/scripts/Script012 - Tabela controle app session.sql
+++ database/scripts/Script012 - Tabela controle app session.sql
@@ -3,7 +3,6 @@ USE `university_market_db`;
 CREATE TABLE `AppSession` (
   `sessionId` INT NOT NULL AUTO_INCREMENT,
   `usuarioId` VARCHAR(150) NOT NULL,
-  `sessionTipoId` INT NOT NULL,
   `sessionToken` VARCHAR(500),
   `dataHoraExpiracao` DATETIME,
   PRIMARY KEY (`sessionId`)
