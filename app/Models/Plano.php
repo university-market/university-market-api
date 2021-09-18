ad948353a1a93156502d33b8cef2a6606577281f
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-09-17 23:18:34 -0300

    entidade model para tabela de plano

---
+++ app/Models/Plano.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Plano extends Model {
+    
+  public $timestamps = false; // Nao registrar data/hora criacao/alteracao
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'planoId' => 'integer',
+    'nome' => 'string'
+  ];
+
+  protected $table = 'Plano';
+  protected $primaryKey = 'planoId';
+
+  protected $planoId; // PK Plano
+  protected $nome;
+}
