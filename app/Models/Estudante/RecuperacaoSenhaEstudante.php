49f68cab7bfce238e4780a8518785636450b3189
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-14 20:31:26 -0300

    model de recuperacao de senha do estudante

---
+++ app/Models/Estudante/RecuperacaoSenhaEstudante.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models\Estudante;
+
+use \Illuminate\Database\Eloquent\Model;
+
+use App\Models\Estudante\Estudante;
+
+class RecuperacaoSenhaEstudante extends Model {
+    
+  public $timestamps = false; // Nao registrar data/hora criacao/alteracao
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'recuperacaoSenhaEstudanteId' => 'integer',
+    'tokenRecuperacao' => 'string',
+    'tempoExpiracao' => 'integer',
+    'email' => 'string',
+    'dataHoraSolicitacao' => 'datetime',
+    'completo' => 'boolean',
+    'estudanteId' => 'integer'
+  ];
+
+  protected $table = 'RecuperacaoSenhaEstudante';
+  protected $primaryKey = 'recuperacaoSenhaEstudanteId';
+
+  protected $recuperacaoSenhaEstudanteId; // PK RecuperacaoSenhaEstudante
+  protected $tokenRecuperacao;
+  protected $tempoExpiracao;
+  protected $email;
+  protected $dataHoraSolicitacao;
+  protected $completo;
+  protected $estudanteId; // FK Estudante
+
+  // Entity Relationships
+
+  /**
+   * Obtem Estudante associado a Recuperacao de Senha
+   */
+  public function estudante()
+  {
+    return $this->hasOne(Estudante::class, 'estudanteId', 'estudanteId');
+  }
+}
