79ba83aefe7c532250c68e36bf9a48adf9eb300d
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-17 00:36:45 -0300

    definicao chamada template email recuperacao senha

--- app/Helpers/Email/EmailTemplateIdentifier.php
+++ app/Helpers/Email/EmailTemplateIdentifier.php
@@ -8,4 +8,10 @@ abstract class EmailTemplateIdentifier {
      * @property string $example Template de exemplo
      */
     public static $example = 'base';
+
+    /**
+     * @property string $recuperarSenha Template com link para recuperação de senha
+     */
+    public static $recuperarSenha = 'conta/recuperarsenha';
+
 }
