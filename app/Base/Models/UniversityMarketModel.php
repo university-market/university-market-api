0bf8517c53440dc0bf539bc1e761e2d53f4e0747
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-31 22:39:25 -0300

    setters e getters para propriedades timestamp - model base

--- app/Base/Models/UniversityMarketModel.php
+++ app/Base/Models/UniversityMarketModel.php
@@ -2,6 +2,10 @@
 
 namespace App\Base\Models;
 
+// Base
+use App\Base\Exceptions\UniversityMarketException;
+
+// Common
 use \Illuminate\Database\Eloquent\Model;
 
 /**
@@ -9,5 +13,47 @@ use \Illuminate\Database\Eloquent\Model;
  * @description Database Model padrão do projeto University Market
  */
 class UniversityMarketModel extends Model {
+
+    /**
+     * Properties for entities timestamp control
+     */
+    /**
+     * Created at default Eloquent database column
+     * @property Datetime $created_at
+     */
+    private $created_at;
+    /**
+     * Updated at default Eloquent database column
+     * @property Datetime $updated_at
+     */
+    private $updated_at;
+    /**
+     * Deleted at default University Market database column - optional column - can be replaced by a flag (`deleted`)
+     * @property Datetime $deleted_at
+     */
+    private $deleted_at;
+
+    private function getCreatedAtValue() {
+        
+        return $this->created_at ?? null;
+    }
+
+    private function getUpdatedAtValue()
+    {
+        return $this->updated_at ?? null;
+    }
+
+    private function getDeletedAtValue()
+    {
+        return $this->deleted_at ?? null;
+    }
+
+    private function setDeletedAtValue($value)
+    {
+        if (is_null($value))
+            throw new UniversityMarketException("Value of deleted_at column on update clause can't be null");
+
+        $this->deleted_at = $value;
+    }
 
 }
