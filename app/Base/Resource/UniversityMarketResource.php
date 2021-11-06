4da6168271e8e8f968d6ae116f098f87d58ccd9c
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-05 23:59:30 -0300

    modelo de recursos disponibilizados pelo sistema

--- app/Base/Resource/UniversityMarketResource.php
+++ app/Base/Resource/UniversityMarketResource.php
@@ -1,3 +1,41 @@
 <?php
 
 namespace App\Base\Resource;
+
+abstract class UniversityMarketResource {
+
+    /**
+     * @property $estudante Recurso do sistema do tipo Estudante
+     */
+    public static $estudante;
+
+    /**
+     * @property $usuario Recurso do sistema do tipo Usuario
+     */
+    public static $usuario;
+
+    /**
+     * @property $publicacao Recurso do sistema do tipo Publicação
+     */
+    public static $publicacao;
+
+    /**
+     * @property $instituicao Recurso do sistema do tipo Instituição
+     */
+    public static $instituicao;
+
+    /**
+     * @property $curso Recurso do sistema do tipo Curso
+     */
+    public static $curso;
+
+    /**
+     * @property $bloqueio Recurso do sistema do tipo Bloqueio
+     */
+    public static $bloqueio;
+
+    /**
+     * @property $movimentacao Recurso do sistema do tipo Movimentação
+     */
+    public static $movimentacao;
+}
