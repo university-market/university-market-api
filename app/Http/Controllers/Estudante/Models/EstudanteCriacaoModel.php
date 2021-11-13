65a73c0b1535bdecbaba3caee5f9b697e80d4fa8
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-13 20:13:12 -0300

    melhoria validações na criação do estudante

--- app/Http/Controllers/Estudante/Models/EstudanteCriacaoModel.php
+++ app/Http/Controllers/Estudante/Models/EstudanteCriacaoModel.php
@@ -19,13 +19,15 @@ class EstudanteCriacaoModel {
 
   public function validar() {
 
-    $passwordMinLength = 6;
+    if (is_null($this->nome) || empty(trim($this->nome))) {
 
-    if (is_null($this->nome) || empty(trim($this->nome)))
       throw new UniversityMarketException("O nome é obrigatório");
+    }
 
-    if (is_null($this->email) || empty(trim($this->email)))
+    if (is_null($this->email) || empty(trim($this->email))) {
+
       throw new UniversityMarketException("O e-mail é obrigatório");
+    }
 
     if (is_null($this->dataNascimento) || empty(trim($this->dataNascimento))) {
 
@@ -33,24 +35,19 @@ class EstudanteCriacaoModel {
     }
     elseif ($this->isMenorIdade($this->dataNascimento)) {
 
-      throw new UniversityMarketException("É necessário ser maior de idade para realizar seu cadastro");
+      throw new UniversityMarketException("É necessário ser maior de idade para se cadastrar");
     }
 
     if (is_null($this->senha) || empty(trim($this->senha))) {
 
       throw new UniversityMarketException("A senha é obrigatória");
     }
-    elseif (strlen(trim($this->senha)) < $passwordMinLength) {
 
-      throw new UniversityMarketException("O tamanho mínimo para a senha é de $passwordMinLength caracteres");
+    if (is_null($this->cursoId) || empty(trim($this->cursoId))) {
+
+      throw new UniversityMarketException("É obrigatório informar o curso");
     }
 
-    if (is_null($this->cursoId) || empty(trim($this->cursoId)))
-      throw new UniversityMarketException("É obrigatório informar o curso");
-
-    // Instituição não é obrigatória no cadastro do estudante
-    // if (is_null($this->instituicaoId) || empty($this->instituicaoId))
-    //   throw new UniversityMarketException("É obrigatório informar a instituição de ensino");
   }
 
   private function isMenorIdade($dataNascimento) {
