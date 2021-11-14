6d7eefd60567bedbebc5a88f6c369dcb1618f446
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-14 02:16:59 -0300

    routing para métodos de recuperacao de senha

--- routes/account.php
+++ routes/account.php
@@ -22,4 +22,16 @@ $router->group(['prefix' => $base, 'namespace' => $namespace], function () use (
     // Obter profile
     $router->get('profile', 'AccountController@profile');
 
+    // Solicitar recuperação de senha
+    $router->post('recuperacaosenha/solicitar', 'AccountController@solicitarRecuperacaoSenha');
+
+    // Validar token de recuperacao de senha
+    $router->get('recuperacaosenha/validar/token/{token}', 'AccountController@validarTokenRecuperacaoSenha');
+
+    // Validar email de recuperacao de senha
+    $router->get('recuperacaosenha/validar/email', 'AccountController@validarEmailRecuperacaoSenha');
+
+    // Alterar senha
+    $router->put('recuperacaosenha/alterar', 'AccountController@alterarSenha');
+
 });
