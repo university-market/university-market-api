1bb7850fddbc52f83df872d1069aa44d1132170f
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-15 18:04:22 -0300

    método para validacao da criacao do estudante

--- app/Http/Controllers/Usuario/Models/CriacaoUserModel.php
+++ app/Http/Controllers/Usuario/Models/CriacaoUserModel.php
@@ -2,73 +2,79 @@
 
 namespace App\Http\Controllers\Usuario\Models;
 
-class CriacaoUserModel {
+use App\Base\Validators\PessoaValidator;
+use App\Base\Exceptions\UniversityMarketException;
 
-    public $name;
-    public $ra;
-    public $telefone;
+class CriacaoUsuarioModel {
+
+    public $nome;
+    public $email;
     public $cpf;
     public $senha;
-    public $email;
-    public $curso;
-    public $dataNasc;
+    public $dataNascimento;
+    public $instituicaoId;
     
-    
-    public function validaremail(){
-        
-        // Remove os caracteres ilegais, caso tenha
-        $email = filter_var($this->email, FILTER_SANITIZE_EMAIL);
-
-        // Valida o e-mail
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return true;
-        } else {
-            throw new \Exception('email invalido !');
-        }
-    }
-
-    public function validaCPF() {
- 
-        // Extrai somente os números
-        $cpf = preg_replace( '/[^0-9]/is', '', $this->cpf );
-         
-        // Verifica se foi informado todos os digitos corretamente
-        if (strlen($cpf) != 11) {
-            throw new \Exception('Cpf invalido !');
-        }
-    
-        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
-        if (preg_match('/(\d)\1{10}/', $cpf)) {
-            throw new \Exception('Cpf invalido !');
-        }
-    
-        // Faz o calculo para validar o CPF
-        for ($t = 9; $t < 11; $t++) {
-            for ($d = 0, $c = 0; $c < $t; $c++) {
-                $d += $cpf[$c] * (($t + 1) - $c);
-            }
-            $d = ((10 * $d) % 11) % 10;
-            if ($cpf[$c] != $d) {
-                throw new \Exception('Cpf invalido!');
-            }
-        }
-        return true;
-    
-    }
-
     public function validar() {
         
-        if (!$this->email && $this->password && $this->name){
-            throw new \Exception('Dados Incompletos!');
+        if (is_null($this->nome) || empty($this->nome)) {
+
+            throw new UniversityMarketException("O nome é obrigatório");
         }
 
-        if($this->validaCPF($this->cpf) != true){
-            throw new \Exception('cpf invalido!');
+        if (is_null($this->email) || empty($this->email)) {
+
+            throw new UniversityMarketException("O e-mail é obrigatório");
         }
 
+        $this->validarEmail();
+
+        if (is_null($this->cpf) || empty($this->cpf)) {
+
+            throw new UniversityMarketException("O CPF é obrigatório");
+        }
+
+        $this->validarCpf();
+
+        if (is_null($this->senha) || empty($this->senha)) {
+
+            throw new UniversityMarketException("A senha é obrigatória");
+        }
+
+        if (is_null($this->dataNascimento) || empty($this->dataNascimento)) {
+
+            throw new UniversityMarketException("A data de nascimento é obrigatória");
+        }
+
+        $this->validarMaioridadeCivil();
+
+        if (is_null($this->instituicaoId) || empty($this->instituicaoId))
+            throw new UniversityMarketException("O cadastro do usuário deve estar relacionado à uma instituição de ensino");
+    }
+
+    /**
+     * Validar e-mail informado na model
+     */
+    private function validarEmail() {
         
-        if($this->validaremail($this->email) != true){
-            throw new \Exception('email invalido!');
-        }
+        if (!PessoaValidator::validarEmail($$this->email))
+            throw new UniversityMarketException("Endereço de e-mail inválido");
+    }
+
+    /**
+     * Validar CPF informado na model
+     */
+    private function validarCpf() {
+ 
+        if (!PessoaValidator::validarCpf($this->cpf))
+            throw new UniversityMarketException("CPF informado não é válido");
+    }
+
+    /**
+     * Validar idade informada na model
+     */
+    private function validarMaioridadeCivil() {
+
+        if (!PessoaValidator::validarMaioridade($this->dataNascimento))
+            throw new UniversityMarketException("É necessário ser maior de idade para se cadastrar no sistema");
     }
 }
