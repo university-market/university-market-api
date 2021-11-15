2fd55bb5be19555a64e567b3b2178c12705b2c7d
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-15 17:24:47 -0300

    repository para usuario - abstração de criacao

---
+++ app/Repositories/Usuario/UsuarioRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories\Usuario;
+
+// Base
+use App\Base\Exceptions\UniversityMarketException;
+use App\Common\Constants\UniversityMarketConstants;
+
+// Models
+use App\Models\Session\BaseSession;
+
+class UsuarioRepository {
+
+    /**
+     * Criação de um usuário dentro do sistema
+     * 
+     * @method createUsuario
+     * 
+     * @return void
+     */
+    public function createUsuario() {
+        
+    }
+
+}
