ec041d10dfb66fa99ae2a3365fac5a0707277ffd
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-21 22:46:05 -0300

    script insercoes tipos denuncia

---
+++ database/scripts/Script5 - Inserts tipo denuncia.sql
@@ -0,0 +1,29 @@
+INSERT INTO 
+`universitymarket_db2`.`tipos_denuncias` (`id`, `descricao`, `created_at`, `updated_at`) 
+VALUES
+('1', 'Produto já foi vendido', '2021-11-09 20:19:55', '2021-11-09 20:19:55');
+
+INSERT INTO 
+`universitymarket_db2`.`tipos_denuncias` (`id`, `descricao`, `created_at`, `updated_at`) 
+VALUES
+('2', 'Preço enganoso ou errado', '2021-11-09 20:19:55', '2021-11-09 20:19:55');
+
+INSERT INTO 
+`universitymarket_db2`.`tipos_denuncias` (`id`, `descricao`, `created_at`, `updated_at`) 
+VALUES
+('3', 'Propaganda indevida ou enganosa', '2021-11-09 20:19:55', '2021-11-09 20:19:55');
+
+INSERT INTO 
+`universitymarket_db2`.`tipos_denuncias` (`id`, `descricao`, `created_at`, `updated_at`) 
+VALUES
+('4', 'Produto proibido, ilegal ou ofensivo', '2021-11-09 20:19:552021-11-09 20:19:55', '2021-11-09 20:19:55');
+
+INSERT INTO 
+`universitymarket_db2`.`tipos_denuncias` (`id`, `descricao`, `created_at`, `updated_at`) 
+VALUES
+('5', 'Suspeita de golpe (fraude) ', '2021-11-09 20:19:55', '2021-11-09 20:19:55');
+
+INSERT INTO 
+`universitymarket_db2`.`tipos_denuncias` (`id`, `descricao`, `created_at`, `updated_at`) 
+VALUES
+('6', 'Outros', '2021-11-09 20:19:55', '2021-11-09 20:19:55');
