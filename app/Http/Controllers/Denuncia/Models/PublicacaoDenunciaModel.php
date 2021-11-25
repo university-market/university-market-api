ada3222c84bd64a0e2021c0fb0335ceedae07654
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-24 22:16:56 -0300

    model de denuncia de publicacao

---
+++ app/Http/Controllers/Denuncia/Models/PublicacaoDenunciaModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Publicacao\Models;
+
+use App\Base\Exceptions\UniversityMarketException;
+
+class PublicacaoDenunciaModel {
+
+  public $estudante_id_autor;
+  public $estudante_id_denunciado;
+  public $publicacao_id;
+  public $motivo;
+  public $tipo_denuncia_id;
+
+  public function validar() {
+
+    if (is_null($this->motivo) || empty($this->motivo))
+        throw new UniversityMarketException("O motivo da denúncia é obrigatório");
+  }
+}
