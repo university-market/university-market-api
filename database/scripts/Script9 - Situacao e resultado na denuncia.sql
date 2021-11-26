1adc884f97ec43341df139bc9fda60513b6c8b1e
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-26 02:51:57 -0300

    inclusao default value para situacao denuncia

--- database/scripts/Script9 - Situacao e resultado na denuncia.sql
+++ database/scripts/Script9 - Situacao e resultado na denuncia.sql
@@ -6,7 +6,7 @@ ALTER TABLE `Denuncias`
 ADD CONSTRAINT `FK_Denuncia_Resultado_Denuncia`
 FOREIGN KEY (`resultado_denuncia_id`) REFERENCES `Resultados_Denuncias`(`id`);
 
-ALTER  TABLE `Denuncias` ADD COLUMN `situacao_denuncia_id` INT NULL;
+ALTER  TABLE `Denuncias` ADD COLUMN `situacao_denuncia_id` INT NOT NULL DEFAULT 1;
 
 ALTER TABLE `Denuncias`
 ADD CONSTRAINT `FK_Denuncia_Situacao_Denuncia`
