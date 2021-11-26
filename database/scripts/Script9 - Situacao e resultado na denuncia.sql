61099e8b1f7e1f84bcf8986067ef655c5eb799ec
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-26 02:49:42 -0300

    inclusao situacao e resultado na tabela denuncias

---
+++ database/scripts/Script9 - Situacao e resultado na denuncia.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `Denuncias` DROP COLUMN `apurada`;
+
+ALTER  TABLE `Denuncias` ADD COLUMN `resultado_denuncia_id` INT NULL;
+
+ALTER TABLE `Denuncias`
+ADD CONSTRAINT `FK_Denuncia_Resultado_Denuncia`
+FOREIGN KEY (`resultado_denuncia_id`) REFERENCES `Resultados_Denuncias`(`id`);
+
+ALTER  TABLE `Denuncias` ADD COLUMN `situacao_denuncia_id` INT NULL;
+
+ALTER TABLE `Denuncias`
+ADD CONSTRAINT `FK_Denuncia_Situacao_Denuncia`
+FOREIGN KEY (`situacao_denuncia_id`) REFERENCES `Situacoes_Denuncias`(`id`);
