c8c4b5d9293509b715f9399266685781a15c805b
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-26 02:42:42 -0300

    ajuste nome tabela situacoes denuncias

--- database/scripts/Script7 - Situacoes denuncias.sql
+++ database/scripts/Script7 - Situacoes denuncias.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `Situacao_Denuncias` (
+CREATE TABLE `Situacoes_Denuncias` (
     `id` INT NOT NULL,
     `situacao` VARCHAR(250) NOT NULL,
     `descricao` VARCHAR(250) NOT NULL,
@@ -6,14 +6,14 @@ CREATE TABLE `Situacao_Denuncias` (
     PRIMARY KEY (`id`)
 );
 
-INSERT INTO `Situacao_Denuncias` (`id`, `situacao`, `descricao`)
+INSERT INTO `Situacoes_Denuncias` (`id`, `situacao`, `descricao`)
 VALUES (1, 'Pendente', 'A denúncia foi enviada e será analisada pelos administradores');
 
-INSERT INTO `Situacao_Denuncias` (`id`, `situacao`, `descricao`)
+INSERT INTO `Situacoes_Denuncias` (`id`, `situacao`, `descricao`)
 VALUES (2, 'Concluída', 'A denúncia foi apurada e as medidas cabíveis foram tomadas');
 
-INSERT INTO `Situacao_Denuncias` (`id`, `situacao`, `descricao`)
+INSERT INTO `Situacoes_Denuncias` (`id`, `situacao`, `descricao`)
 VALUES (3, 'Arquivada', 'A denúncia foi arquivada pois a publicação foi reformulada pelo autor');
 
-INSERT INTO `Situacao_Denuncias` (`id`, `situacao`, `descricao`)
+INSERT INTO `Situacoes_Denuncias` (`id`, `situacao`, `descricao`)
 VALUES (4, 'Excluída', 'A denúncia foi excluída pois não apresentou coerência nas informações');
